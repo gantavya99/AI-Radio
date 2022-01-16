dd57d5e14ea546d9c3be6801e03e816689ed0feb
Gantavya Saraswat <gantavya99@gmail.com>
2022-01-16 11:10:30 +0530

    added radip class with all the parameters

--- ai_radio/lib/main.dart
+++ ai_radio/lib/main.dart
@@ -1,15 +1,7 @@
+import 'package:ai_radio/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(new MaterialApp(home:homePageState()));
 }
-class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      
-    );
-  }
-}
---
+++ ai_radio/lib/pages/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:ai_radio/utils/ai_util.dart';
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+import 'package:flutter/material.dart';
+
+class homePageState extends StatefulWidget {
+  const homePageState({ Key? key }) : super(key: key);
+
+  @override
+  _homePageStateState createState() => _homePageStateState();
+}
+
+class _homePageStateState extends State<homePageState> {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: AIColors.primaryColor1,
+    );
+  }
+}
